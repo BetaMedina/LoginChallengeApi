35042059eaedb8a8a0eb35525ba76126af6d51c0
BetaMedina <36626531+BetaMedina@users.noreply.github.com>
2020-01-26 05:58:55 -0300

    Port heroku

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-console.log(`Server ouvindo a porta -> ${process.env.APP_PORT}`);
-_app2.default.listen(process.env.APP_PORT);
+console.log(`Server ouvindo a porta -> ${process.env.PORT}`);
+_app2.default.listen(process.env.PORT || process.env.APP_PORT);
